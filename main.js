5523c056a4d1912711138ec46d1f7cb70109d832
andrey.popov <andrey.popov>
2016-10-26 16:47:03 +0300

    dd

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const port = 5000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('Hello Worldd\n');
 });
 
 server.listen(process.env.PORT || port);
